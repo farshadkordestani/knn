136f4914804882c9b58b10fb169f61d9f55529d8
Farshad <75391746+farshadkordestani@users.noreply.github.com>
2021-05-16 18:42:42 +0430

    Update knn.py

--- knn.py
+++ knn.py
@@ -78,3 +78,61 @@ model.fit(x_train, y_train)
 predictions = model.predict(x_test)
 print('Scikit accuracy:', accuracy_score(y_test, predictions))
 
+##########################################################################################
+from math import inf
+class KNN:
+    def __init__(self, k):
+        self.k = k
+
+    def euclid_distance(self, a, b):
+        s = 0
+        for ind in range(len(a)):
+            s += (a[ind] - b[ind]) ** 2
+        return s ** 0.5
+
+    def find_minimum(self, distances):
+        min_val, min_ind = distances[0], 0
+        for ind in range(1, len(distances)):
+            if distances[ind] < min_val:
+                min_val, min_ind = distances[ind], ind
+        return min_ind
+
+    def fit(self, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def predict(self, x_test):
+        predicts = []
+        for sample in x_test:
+            distances = []
+            for row in self.x_train:
+                distances.append(self.euclid_distance(sample, row))
+            votes = {}
+            for _ in range(self.k):
+                min_ind = self.find_minimum(distances)
+                v =self.y_train[min_ind]
+                votes[v] = votes.get(v, 0) + 1
+                distances[min_ind] = inf
+            min_ind, min_val = 0, 0
+            for key, value in votes.items():
+                if value > min_val:
+                    min_ind, min_val = key, value
+            predicts.append(min_ind)
+        return predicts
+
+
+
+from sklearn.datasets import load_wine as load
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+x_train, x_test, y_train, y_test = train_test_split(load().data, load().target, test_size=0.2, shuffle=True)
+
+
+model = KNN(k=5)
+model.fit(x_train, y_train)
+predictions = model.predict(x_test)
+print('My accuracy:', accuracy_score(y_test, predictions))
+
+
